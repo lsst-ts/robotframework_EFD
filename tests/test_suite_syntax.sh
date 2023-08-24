7019255f8d3f246071654d8141f880f031c993ba
rbovill <rbovill@lsst.org>
2023-08-24 14:37:22 -0700

    Added a couple of pseudo-unit-test scripts, tests/test_suite_syntax.sh and tests/test_base_teststand_config.sh that will help validate reporting harness syntax.

---
+++ tests/test_suite_syntax.sh
@@ -0,0 +1,26 @@
+cd ../
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Offline.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Standby.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Disabled.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Enabled.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Housekeeping.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_MainTel_Housekeeping.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Image_Verification.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_LATISS_Checkout.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Telescope_Dome_Checkout.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Prep_Flat.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Flat_Calibrations.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_PTC_Calibrations.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Prep_Onsky.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_WEP_Align.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Acq_Take_Seq_POINTING.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Acq_Take_Seq_VERIFY.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Acq_Take_Seq_TEST.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Acq_Take_Seq_NOMINAL.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Stop.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_AuxTel_Shutdown.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Enable_ATCS.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_BigCamera_Image_Verification.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_BigCamera_Calibrations.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_LOVE_Stress_Test.list
+robot -A Vars.txt -d Reports/dryrun --variable env_efd:tucson_teststand_efd --dryrun -A Test_Report_Shutdown.list
